91b5c9edfad7e41c2fe21335efaaad81b7ac795b
Mong Cha <info.mongcha@gmail.com>
2024-08-22 23:53:20 +0600

    Descending number print 1000 to 1

---
+++ subeen-52-problem/3-descending-numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, count = 0;
+
+    // Print 1000 to 1 Descending number
+    for (i = 1000; i >= 1; i--)
+    {
+        printf("%d\t", i);
+
+        // Increase count value
+        count++;
+        // Each line 5 output number only can show
+        if (count == 5)
+        {
+            printf("\n");
+            count = 0;
+        }
+    }
+
+    return 0;
+}
