13c5b33365871391c738308bdfba51d6246e3391
Mong Cha <info.mongcha@gmail.com>
2024-09-02 17:15:07 +0600

    Divisors number print

--- subeen-52-problem/4-divisor-number.c
+++ subeen-52-problem/4-divisor-number.c
@@ -1,15 +1,27 @@
 
+/*
+* Description of the problem
+- Find all factors (divisors) of a number.
+
+*Input
+- The first line will contain number of test cases T(<=10). Next T-number of lines will contain 1 integer N each, where 1 <= N <= 5,.. number.
+
+*Output
+- One line should be printed for each case, beginning with the case number. Then print all factors of N in ascending order. Each factor should be printed only once. Factors must be separated 1 space only. There will be no extra spaces at the end of the line.
+
+*/
+
 #include <stdio.h>
 
 int main()
 {
-    int line_limitNum, i, j, userNum;
+    int N_limitNum, i, j, userNum;
 
     // Input the number test cases line
-    printf("How many number you want: ");
-    scanf("%d", &line_limitNum);
+    printf("How many divisors you want ? : ");
+    scanf("%d", &N_limitNum);
 
-    for (i = 1; i <= line_limitNum; i++)
+    for (i = 1; i <= N_limitNum; i++)
     {
         // Input user divisor number
         printf("Enter your check number: ");
